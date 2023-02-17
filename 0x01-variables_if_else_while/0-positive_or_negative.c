06d85225b43e736522c0aab47bf0bffa1be5d3a6
Ijeoma Mokogwu <Ijeomamokogwu@gmail.com>
2023-02-17 15:42:22 -0800

    program that completes if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,20 +6,17 @@
 int main(void)
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-if ( n>0 ){
-printf("is positive\n" );
+if (n>0){
+printf("%d is positive\n",n);
 }
-
-if ( n==0 ){
-printf("is zero\n" );	
+if (n==0){
+printf("%d is zero\n",n);	
 }
-
-if ( n<0 ){
-printf("is negative\n" );
+if (n<0){
+printf("%d is negative\n",n);
 }
 return (0);
 }
