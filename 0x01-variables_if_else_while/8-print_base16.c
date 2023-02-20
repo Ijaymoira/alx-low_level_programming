15cc93a70d1e1c0e1c027b063abbd33e29ba4195
Ijeoma Mokogwu <Ijeomamokogwu@gmail.com>
2023-02-20 02:46:01 -0800

    printing the first digits of base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,11 +6,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 int x;
 char i;
-for (x = '0'; x < 10; x++)
+for (x = '0'; x <= '9'; x++)
 putchar(x);
 for (i = 'a'; i <= 'f'; i++)
 putchar(i);
