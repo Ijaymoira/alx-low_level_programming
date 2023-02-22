4be8c26813b81c38a570d4c903b35b8cbaddd6f1
Ijeoma Mokogwu <Ijeomamokogwu@gmail.com>
2023-02-22 09:08:51 -0800

    putchat plus strings

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,11 +4,11 @@
  * main - entry point
  *
  * Description - displaying a string putchar using putchar
- *
+ * *s is string
  * Return: Always 0 (Success)
  */
 void first_strin(char *s);
-int main()
+int main(void)
 {
 char s[] = "putchar";
 putchar('_');
