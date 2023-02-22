e7b4ceaaebb01708c6da00ed86164679101dc75e
Ijeoma Mokogwu <Ijeomamokogwu@gmail.com>
2023-02-22 09:13:09 -0800

    putchat plus strings

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,8 @@
  * main - entry point
  *
  * Description - displaying a string putchar using putchar
- * *s is string
- * Return: Always 0 (Success)
+ * Description - the arguemtn first_strin is the loop for arg s
+ *  Return: Always 0 (Success)
  */
 void first_strin(char *s);
 int main(void)
