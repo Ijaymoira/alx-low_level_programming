d8f305f975772179ff38493186b230a751df2837
Ijeoma Mokogwu <Ijeomamokogwu@gmail.com>
2023-03-07 20:43:57 -0800

    final correction for functions 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,7 +6,6 @@
  * Desc: Header file containing prototypes for all functions
  * used in the 0x06-pointers_arrays_strings directory.
  */
-#include <stddef.h>
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
