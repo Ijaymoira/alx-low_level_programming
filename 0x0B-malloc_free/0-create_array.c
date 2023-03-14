6b3575feaf80e11298e98c3ea9e4a805375f6be9
Ijeoma Mokogwu <Ijeomamokogwu@gmail.com>
2023-03-14 16:59:25 -0700

    functions to create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,10 @@
  */
 char *create_array(unsigned int size, char c)
 {
+char *ptr;
+unsigned int i;
 if (size == 0)
 return (NULL);
-unsigned int i;
-char *ptr;
 ptr = malloc(sizeof(char) * size);
 if (ptr == NULL)
 return (NULL);
