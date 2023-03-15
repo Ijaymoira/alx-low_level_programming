db8fb45062741a9f95ad587dd010ed719c9689f9
Ijeoma Mokogwu <Ijeomamokogwu@gmail.com>
2023-03-15 16:15:22 -0700

    creating a library

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * create_array - function that creates an array of chars
+ * and initializes it with a specific char.
+ * @c: initialized chairacter in array
+ * @size: the size of the memory
+ * Return: Returns NULL if size = 0
+ * or pointer to the array, or NULL if it fails
+ * main - initializing the create_array function
+ */
+int _strcmp(char *s1, char *s2)
+{
+}
