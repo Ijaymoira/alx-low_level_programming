bf5cc45fa76891cd86da0467fca7944d271a5a68
Ijeoma Mokogwu <Ijeomamokogwu@gmail.com>
2023-03-15 19:12:28 -0700

    nname only

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -  writes its name
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
+int main(int __attribute__((unused)) argc, char **argv)
+{
+printf("%s\n", argv[0]);
+return (0);
+}
