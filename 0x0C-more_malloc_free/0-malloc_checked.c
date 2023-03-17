32f4b6ff2e8ee73e8ee7565f908d8db96e694ef6
Ijeoma Mokogwu <Ijeomamokogwu@gmail.com>
2023-03-17 16:54:57 -0700

    memory allocation using malloc and succesful termination using exit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * Description - returns a pointer to the allocated memory
+ * @b: size of memory to be allocated
+ * Return: always 0
+ */
+void *malloc_checked(unsigned int b)
+{
+int *ptr;
+ptr = malloc(sizeof(int) * b);
+return (ptr);
+if (ptr == NULL)
+exit(98);
+putchar('\n');
+}
