5a734c691edeed5b5ab81d30722ec44ee81c1221
Ijeoma Mokogwu <Ijeomamokogwu@gmail.com>
2023-03-21 21:17:36 -0700

    Initializing a struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ *
+ *
+ *
+ */
+void print_dog(struct dog *d)
+{
+struct dog my_dog(char *name, float age, char *owner);
+d = (struct)malloc(sizeof(struct dog));
+d = &my_dog;
+d -> name = my_dog.name;
+d -> age = my_dog.age;
+d -> owner = my_dog.owner
+if (d == NULL)
+printf("nil");
+if (name == NULL )
+printf("Name: nil");
+if (d == NULL)
+return;
+}
