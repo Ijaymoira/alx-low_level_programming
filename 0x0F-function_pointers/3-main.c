863f47d3e8e649795ef5aa6a0599ddc27a1c6795
Ijeoma Mokogwu <Ijeomamokogwu@gmail.com>
2023-03-24 20:15:05 -0700

    forming a calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,36 +14,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int one, two, ans;
-	int (*res)(int, int);
-	char *get_op;
-
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	one = atoi(argv[1]);
-	two = atoi(argv[3]);
-	get_op = argv[2];
-
-	/* added edge case if argv[2] was longer than 1 char*/
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	res = get_op_func(get_op);
-	ans = res(one, two);
-
-	printf("%d\n", ans);
-	return (0);
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2]);
+int x = 0;
+if (argc != 4)
+printf("error\n");
+exit(98);
+if ((('+') || ('-') || ('*') || ('/') || ('%')) == (0))
+printf("error\n");
+exit(99);
+if (((num1 && num2) == 0) && (('/' || '%') / (x)))
+printf("error\n");
+exit(100);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,26 +2,66 @@
 #include <stdio.h>
 #include "3-calc.h"
 /**
- * get_op_func - matches operator from main
- * @s: op str
- * Return: 0
+ * op_add - returns sum of a and b
+ * @a: the first integer
+ * @b: the second integer
+ * Return: The sum of a and b
  */
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
 {
-op_t op_s[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i = 0;
-while (op_s[i].op)
-{
-if (*(op_s[i].op) == *s)
-return (op_s[i].f);
-i++;
+int sum;
+sum = a + b;
+return (sum);
 }
-return (NULL);
+/**
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+int difference;
+difference = a - b;
+return (difference);
+}
+/**
+ * op_mul - Returns the multiplication of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The multiplication of a and b.
+ */
+int op_mul(int a, int b)
+{
+int multiply;
+multiply = a * b;
+return (multiply);
+}
+/**
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The division of a and b.
+ */
+int op_div(int a, int b)
+{
+int division;
+division = a / b;
+return (division);
+}
+/**
+ * op_mod - Returns the modulus of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The modulus of a and b.
+ */
+int op_mod(int a, int b)
+{
+int modulus;
+modulus = a % b;
+return (modulus);
 }
