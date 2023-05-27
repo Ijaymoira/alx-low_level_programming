13868118466dbf6f59e526d4973712282001c646
Ijaymoira <Ijeomamokogwu@gmail.com>
2023-05-27 02:42:17 +0100

    creating a dynamic library1

--- 0x18-dynamic_libraries/1-isdigit.c
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -1,13 +1,1 @@
-#include "main.h"
-/**
- * _isdigit - function that verifies numbers are 0 - 9
- * @c: char to be chaecked
- * Return: returns 1 or 0
- */
-int _isdigit(int c)
-{
-if ((c >= '0') && (c <= '9'))
-return (1);
-else
-return (0);
-}
+int _isdigit(int c);
