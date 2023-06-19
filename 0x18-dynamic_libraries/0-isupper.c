435432dce42305648dd45519ad348e8c969485ca
Ijaymoira <Ijeomamokogwu@gmail.com>
2023-06-19 23:47:53 +0100

    Creating dynamic/shared library to libdymanic.so

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,1 +1,14 @@
-int _isupper(int c);
+#include "main.h"
+#include <stdio.h>
+/**
+ *_isupper - checks if a char is uppercase
+*@c: interger c passed in as a parameter
+ *Return: returns 1 if true and 0 if otherwise
+ */
+int _isupper(int c)
+{
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+return (0);
+}
+
--- 0x18-dynamic_libraries/3-islower.c
+++ 0x18-dynamic_libraries/3-islower.c
@@ -1,1 +1,13 @@
-int _islower(int c);
+#include "main.h"
+#include <stdio.h>
+/**
+*_islower - checks for lower characters
+*@c: characters to be verified
+*Return: return 0
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+return (0);
+}
