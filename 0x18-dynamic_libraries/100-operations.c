572d7f6ff939055b05e5fe06f9c9169929d9343b
Ijaymoira <Ijeomamokogwu@gmail.com>
2023-06-20 05:40:23 +0100

    Create a dynamic library that contains C functions that can be called from Python. See example for more detail

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    int a;
+    int b;
+    int sum = a + b;
+    return sum;
+}
